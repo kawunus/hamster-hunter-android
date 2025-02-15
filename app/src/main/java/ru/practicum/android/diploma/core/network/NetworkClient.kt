828a09a87a2070bd9210c6c7ddbd8fd8859e01b7
stanislau <s.markovtsev@gmail.com>
2025-02-15 16:27:03 +0300

    GH-3: перенес Response в отдельный пакет

--- app/src/main/java/ru/practicum/android/diploma/core/network/NetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/network/NetworkClient.kt
@@ -1,5 +1,7 @@
 package ru.practicum.android.diploma.core.network
 
+import ru.practicum.android.diploma.core.network.dto.Response
+
 
 interface NetworkClient {
     suspend fun doRequest(dto:Any) : Response
--- app/src/main/java/ru/practicum/android/diploma/core/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/network/RetrofitNetworkClient.kt
@@ -1,5 +1,7 @@
 package ru.practicum.android.diploma.core.network
 
+import ru.practicum.android.diploma.core.network.dto.Response
+
 class RetrofitNetworkClient(private val hhApiService: HHApiService) : NetworkClient {
 
     override suspend fun doRequest(dto: Any): Response {
