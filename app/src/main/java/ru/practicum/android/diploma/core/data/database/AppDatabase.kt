0a80063c55df0432fdf280266829c1ae023efe07
stanislau <s.markovtsev@gmail.com>
2025-02-15 16:44:11 +0300

    GH-3: Сделал временный класс PlaceholderEntity, для того, чтобы проект собирался

--- app/src/main/java/ru/practicum/android/diploma/core/data/database/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/data/database/AppDatabase.kt
@@ -2,9 +2,10 @@ package ru.practicum.android.diploma.core.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import ru.practicum.android.diploma.core.data.database.entity.PlaceholderEntity
 
 @Database(
     version = 1,
-    entities = []
+    entities = [PlaceholderEntity::class]
 )
 abstract class AppDatabase : RoomDatabase()
