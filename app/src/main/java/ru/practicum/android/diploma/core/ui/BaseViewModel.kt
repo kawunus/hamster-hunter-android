6c3ce23457e23cb94ea9793e1b13ef8b329fecb6
stanislau <s.markovtsev@gmail.com>
2025-02-15 10:32:37 +0300

    GH-13: Добавлен базовый класс BaseViewModel

---
+++ app/src/main/java/ru/practicum/android/diploma/core/ui/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package com.kawunus.playlistmaker.core.ui
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Базовая ViewModel. Необходима для реализации базового фрагмента
+ *
+ */
+abstract class BaseViewModel() : ViewModel()
