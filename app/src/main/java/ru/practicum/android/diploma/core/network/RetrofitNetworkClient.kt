0df0a064abc1fcadf9efec7a1659fe4ff05cdeb1
stanislau <s.markovtsev@gmail.com>
2025-02-15 16:35:04 +0300

    GH-3: поправил кодстайл

--- app/src/main/java/ru/practicum/android/diploma/core/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/network/RetrofitNetworkClient.kt
@@ -4,7 +4,12 @@ import ru.practicum.android.diploma.core.network.dto.Response
 
 class RetrofitNetworkClient(private val hhApiService: HHApiService) : NetworkClient {
 
+    companion object {
+        private const val HTTP_BAD_REQUEST = 400
+    }
+
     override suspend fun doRequest(dto: Any): Response {
-        return Response().apply { resultCode = 400 }
+        return Response().apply { resultCode = HTTP_BAD_REQUEST }
     }
 }
+
