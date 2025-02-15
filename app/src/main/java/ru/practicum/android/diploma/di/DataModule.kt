2a1c37e9e8df300112974f75f6e27db4f1480545
stanislau <s.markovtsev@gmail.com>
2025-02-15 16:14:58 +0300

    GH-3: Прокинул sharedPreferences в dataModule

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.di
 
+import android.content.Context
 import androidx.room.Room
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -8,6 +9,10 @@ import ru.practicum.android.diploma.core.database.AppDatabase
 val dataModule = module {
 
     single {
-        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "hamster_hunter_db").build()
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "hamster_hunter_database.db").build()
+    }
+
+    single {
+        androidContext().getSharedPreferences("hamster_hunter_shared_preferences", Context.MODE_PRIVATE)
     }
 }
