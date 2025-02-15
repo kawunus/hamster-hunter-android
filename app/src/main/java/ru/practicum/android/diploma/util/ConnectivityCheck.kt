8f589e5380e6c5a44ed47662cf616810e9939dd5
Александр А. <laorha@yandex.ru>
2025-02-15 16:54:08 +0500

    GH-4: Исправлен кодстайл, функция isNetworkAvailable теперь содержит два return вместо трех

--- app/src/main/java/ru/practicum/android/diploma/util/ConnectivityCheck.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/ConnectivityCheck.kt
@@ -8,10 +8,11 @@ object ConnectivityCheck {
 
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val network = connectivityManager.activeNetwork ?: return false
+        val network = connectivityManager.activeNetwork
 
-        val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
-
-        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        return network?.let {
+            val networkCapabilities = connectivityManager.getNetworkCapabilities(it)
+            networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+        } ?: false
     }
 }
