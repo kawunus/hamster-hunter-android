42f6efbabe2c3683fbeac701a3e79600cf78d5ee
stanislau <s.markovtsev@gmail.com>
2025-02-15 16:24:43 +0300

    GH-3: Прокинул HHApiService в dataModule

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -4,9 +4,21 @@ import android.content.Context
 import androidx.room.Room
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import ru.practicum.android.diploma.core.data.database.AppDatabase
+import ru.practicum.android.diploma.core.network.HHApiService
 
 val dataModule = module {
+
+    single<HHApiService> {
+        val hhBaseUrl = ""
+        Retrofit.Builder()
+            .baseUrl(hhBaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HHApiService::class.java)
+    }
 
     single {
         Room.databaseBuilder(androidContext(), AppDatabase::class.java, "hamster_hunter_database.db").build()
