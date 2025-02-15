1f027804b559e4fe65a54055c3aaf7079ec317ba
stanislau <s.markovtsev@gmail.com>
2025-02-15 16:37:18 +0300

    GH-3: поправил кодстайл

--- app/src/main/java/ru/practicum/android/diploma/core/network/NetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/network/NetworkClient.kt
@@ -3,5 +3,5 @@ package ru.practicum.android.diploma.core.network
 import ru.practicum.android.diploma.core.network.dto.Response
 
 interface NetworkClient {
-    suspend fun doRequest(dto: Any) : Response
+    suspend fun doRequest(dto: Any): Response
 }
