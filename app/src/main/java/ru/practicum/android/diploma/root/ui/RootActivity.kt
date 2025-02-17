bc9c54b38ba3496e9da6c09990f59ca25b1c9f49
Александр А. <laorha@yandex.ru>
2025-02-17 23:47:18 +0500

    GH-9: Исправления согласно ревью, удалил неиспользуемые стили, добавил функции расширения для отображения View, исправил стиль написания id

--- app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -1,7 +1,7 @@
 package ru.practicum.android.diploma.root.ui
 
+import android.view.View
 import androidx.core.bundle.Bundle
-import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.R
@@ -13,6 +13,7 @@ class RootActivity : BaseActivity<ActivityRootBinding>(ActivityRootBinding::infl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
         initViews()
     }
 
@@ -23,6 +24,7 @@ class RootActivity : BaseActivity<ActivityRootBinding>(ActivityRootBinding::infl
     private fun bottomNavigationManager() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
         val navController = navHostFragment.navController
+
         binding.bottomNavigationView.setupWithNavController(navController)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
@@ -35,13 +37,21 @@ class RootActivity : BaseActivity<ActivityRootBinding>(ActivityRootBinding::infl
     }
 
     private fun hideBottomNavigation() {
-        binding.bottomNavigationView.isVisible = false
-        binding.divider.isVisible = false
+        binding.bottomNavigationView.gone()
+        binding.divider.gone()
     }
 
     private fun showBottomNavigation() {
-        binding.bottomNavigationView.isVisible = true
-        binding.divider.isVisible = true
+        binding.bottomNavigationView.visible()
+        binding.divider.visible()
+    }
+
+    fun View.gone() {
+        this.visibility = View.GONE
+    }
+
+    fun View.visible() {
+        this.visibility = View.VISIBLE
     }
 }
 
