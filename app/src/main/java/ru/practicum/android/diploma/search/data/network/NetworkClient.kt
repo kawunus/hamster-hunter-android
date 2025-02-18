dcd88b5a02a78bfcbb75a0e784b4953aef68f398
Svitlana Filippova <s.rubinets@gmail.com>
2025-02-18 20:00:14 +0300

    GH-28 корректировка импорта класса Response

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/NetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/NetworkClient.kt
@@ -1,6 +1,6 @@
 package ru.practicum.android.diploma.search.data.network
 
-import ru.practicum.android.diploma.search.data.dto.Response
+import ru.practicum.android.diploma.core.network.dto.Response
 
 interface NetworkClient {
     suspend fun doRequest(dto: Any): Response
