020b9206d69e44d63a7ac4b70d051eeab236d02b
User <artemenkoe2711@gmail.com>
2025-02-18 15:27:16 +0400

    Написал верстку экрана "Поиск", а так же реализовал отображение иконок очистки текста в EditText. Настроил логику удаления текста при нажатии на buttonClear. + Добавил Плейсхолдеры, которые находятся на экране "Поиск"

--- app/src/main/java/ru/practicum/android/diploma/search/presentation/ui/fragment/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/ui/fragment/SearchFragment.kt
@@ -1,6 +1,9 @@
 package ru.practicum.android.diploma.search.presentation.ui.fragment
 
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.viewModels
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.core.ui.BaseFragment
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.search.presentation.viewmodel.SearchViewModel
@@ -11,6 +14,43 @@ class SearchFragment : BaseFragment<FragmentSearchBinding, SearchViewModel>(
     override val viewModel: SearchViewModel by viewModels()
 
     override fun initViews() {
+        setupSearchTextWatcher()
+        setupClearButtonClickListener()
+    }
+
+    //настройка отслеживания изменений текста
+    private fun setupSearchTextWatcher() {
+        binding.edittextSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                updateClearButtonIcon(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    //обновление иконки кнопки
+    private fun updateClearButtonIcon(text: CharSequence?) {
+        binding.buttonClear.setImageResource(
+            if (text?.isNotEmpty() == true) R.drawable.edit_text_clear_button
+            else R.drawable.ic_search
+        )
+    }
+
+    // настройка слушателя нажатий
+    private fun setupClearButtonClickListener() {
+        binding.buttonClear.setOnClickListener {
+            handleClearButtonClick()
+        }
+    }
+
+    // обработка нажатия на кнопку очистки
+    private fun handleClearButtonClick() {
+        if (binding.edittextSearch.text.isNotEmpty()) {
+            binding.edittextSearch.text.clear()
+        }
     }
 
     override fun subscribe() {
