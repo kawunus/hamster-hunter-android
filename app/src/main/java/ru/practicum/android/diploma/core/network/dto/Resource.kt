d6af5b5e34056198e3fc5d0c190f23cef115b024
Svitlana Filippova <s.rubinets@gmail.com>
2025-02-19 06:36:31 +0300

    GH-28 правки util-класса Resource для упрощения работы с ним

--- app/src/main/java/ru/practicum/android/diploma/core/network/dto/Resource.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/network/dto/Resource.kt
@@ -1,7 +1,6 @@
 package ru.practicum.android.diploma.core.network.dto
 
-sealed class Resource<T>(val data: T, val message: String? = null, val resultCode: Int) {
-    class Success<T>(data: T, resultCode: Int) : Resource<T>(data = data, resultCode = resultCode)
-    class Error<T>(message: String, data: T, resultCode: Int) :
-        Resource<T>(data = data, message = message, resultCode = resultCode)
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
 }
