319b2f6505ffb6566e0bf30d18617a6644cfdc07
Svitlana Filippova <s.rubinets@gmail.com>
2025-02-20 14:36:36 +0300

    GH-28 кодстайл

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -4,11 +4,8 @@ import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 import ru.practicum.android.diploma.search.data.impl.VacanciesSearchRepositoryImpl
-
 import ru.practicum.android.diploma.search.domain.api.VacanciesSearchRepository
 
-
 val repositoryModule = module {
-
     factoryOf(::VacanciesSearchRepositoryImpl) { bind<VacanciesSearchRepository>() }
 }
