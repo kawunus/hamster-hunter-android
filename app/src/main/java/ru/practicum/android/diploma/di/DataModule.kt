37e46341bb5efed3dd8a2273342c7fc92a0f48f1
Svitlana Filippova <s.rubinets@gmail.com>
2025-02-20 14:32:53 +0300

    GH-28 фикс di для RetrofitNetworkClient

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Room
 import kotlinx.coroutines.flow.MutableSharedFlow
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.practicum.android.diploma.core.data.database.AppDatabase
 import ru.practicum.android.diploma.core.data.network.HHApiService
+import ru.practicum.android.diploma.core.data.network.NetworkClient
 import ru.practicum.android.diploma.core.data.network.RetrofitNetworkClient
 import ru.practicum.android.diploma.search.data.dto.VacanciesSearchRequest
 import ru.practicum.android.diploma.search.data.network.VacanciesPagingSource
@@ -36,7 +38,7 @@ val dataModule = module {
         androidContext().getSharedPreferences(HH_SHARED_PREFS_NAME, Context.MODE_PRIVATE)
     }
 // NetworkClient
-    singleOf(::RetrofitNetworkClient)
+    singleOf(::RetrofitNetworkClient) { bind<NetworkClient>() }
 
 // VacanciesPagingSource
     factory { (searchRequest: VacanciesSearchRequest, foundCount: MutableSharedFlow<Int?>) ->
