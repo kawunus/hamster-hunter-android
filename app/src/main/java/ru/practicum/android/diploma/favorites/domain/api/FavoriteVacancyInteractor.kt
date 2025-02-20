2501a3ed9fedaf0f51e6ded53dacce80155af505
stanislau <s.markovtsev@gmail.com>
2025-02-20 23:53:41 +0300

    GH-51: Добавил интерфейсы для репозитория и интерактора для избранных вакансий

---
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/api/FavoriteVacancyInteractor.kt
@@ -0,0 +1,15 @@
+package ru.practicum.android.diploma.favorites.domain.api
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.search.domain.model.Vacancy
+
+interface FavoriteVacancyInteractor {
+
+    fun getFavoriteVacancies(): Flow<List<Vacancy>>
+
+    suspend fun addVacancyToFavorites(vacancy: Vacancy)
+
+    suspend fun deleteVacancyFromFavorites(vacancy: Vacancy)
+
+    suspend fun isVacancyInFavorites(vacancyId: String): Boolean
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/api/FavoriteVacancyRepository.kt
@@ -0,0 +1,16 @@
+package ru.practicum.android.diploma.favorites.domain.api
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.favorites.data.entity.FavoriteVacancyEntity
+import ru.practicum.android.diploma.search.domain.model.Vacancy
+
+interface FavoriteVacancyRepository {
+
+    fun getFavoriteVacancies(): Flow<List<Vacancy>>
+
+    suspend fun addVacancyToFavorites(vacancy: Vacancy)
+
+    suspend fun deleteVacancyFromFavorites(vacancy: Vacancy)
+
+    suspend fun isVacancyInFavorites(vacancyId: String): Boolean
+}
