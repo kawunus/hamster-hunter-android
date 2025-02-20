b90144a4f7086fe5da0ea6deabe50e1d25442c71
Svitlana Filippova <s.rubinets@gmail.com>
2025-02-20 11:34:33 +0300

    GH-29 фикс краша из-за конвертера VacancyDTo to Domain

--- app/src/main/java/ru/practicum/android/diploma/search/data/mapper/Converters.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/mapper/Converters.kt
@@ -3,31 +3,16 @@ package ru.practicum.android.diploma.search.data.mapper
 import ru.practicum.android.diploma.search.data.dto.VacancyDto
 import ru.practicum.android.diploma.search.domain.model.Vacancy
 
+
 fun VacancyDto.toDomain(): Vacancy {
     return Vacancy(
         id = id ?: "",
         name = name ?: "",
-        company = getEmployerName(),
-        currency = getSalaryCurrency(),
+        company = employer?.name ?: "",
+        currency = salary?.currency ?: "",
         salaryFrom = salary?.from,
         salaryTo = salary?.to,
-        area = getAreaName(),
-        icon = getEmployerLogoUrl()
+        area = area?.name ?: "",
+        icon = employer?.logoUrls?.logoUrl ?: ""
     )
-}
-
-private fun VacancyDto.getEmployerName(): String {
-    return employer?.name ?: ""
-}
-
-private fun VacancyDto.getSalaryCurrency(): String {
-    return salary?.currency ?: ""
-}
-
-private fun VacancyDto.getAreaName(): String {
-    return area?.name ?: ""
-}
-
-private fun VacancyDto.getEmployerLogoUrl(): String {
-    return employer?.logoUrls?.logoUrl ?: ""
 }
