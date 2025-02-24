72bb5fe43318a586cf3d3f4904413d24828535b0
SergeySh97 <Sergey_Sh97@mail.ru>
2025-02-24 12:09:30 +0300

    GH-45: Изменение всех параметров на нулабельные

--- app/src/main/java/ru/practicum/android/diploma/vacancy/data/network/model/VacancyByIdResponse.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/data/network/model/VacancyByIdResponse.kt
@@ -4,16 +4,16 @@ import com.google.gson.annotations.SerializedName
 import ru.practicum.android.diploma.core.data.network.dto.Response
 
 data class VacancyByIdResponse(
-    val name: String,
+    val name: String?,
     val salary: Salary?,
-    val employer: Employer,
-    val area: Area,
-    val experience: Experience,
+    val employer: Employer?,
+    val area: Area?,
+    val experience: Experience?,
     @SerializedName("employment_form")
-    val employment: Employment,
+    val employment: Employment?,
     @SerializedName("work_format")
-    val workFormat: List<WorkFormat>,
-    val description: String,
+    val workFormat: List<WorkFormat>?,
+    val description: String?,
     @SerializedName("key_skills")
-    val keySkills: List<KeySkills>
+    val keySkills: List<KeySkills>?
 ) : Response()
