f4b4ae0c1a69cba661d6aa9045883a5d3e480699
SergeySh97 <Sergey_Sh97@mail.ru>
2025-02-24 12:09:47 +0300

    GH-45: Добавление обработок нулабельных параметров

--- app/src/main/java/ru/practicum/android/diploma/vacancy/mapper/Mapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/mapper/Mapper.kt
@@ -6,16 +6,16 @@ import ru.practicum.android.diploma.vacancy.domain.model.VacancyDetails
 object Mapper {
 
     fun VacancyByIdResponse.toVacancyDetails() = VacancyDetails(
-        name = name,
+        name = name.orEmpty(),
         salaryFrom = salary?.from.toString(),
         salaryTo = salary?.to.toString(),
-        employer = employer.name.orEmpty(),
-        area = area.name,
-        experience = experience.name,
-        employment = employment.name,
-        workFormat = workFormat.map { it.name },
-        description = description,
-        keySkills = keySkills.map { it.name },
-        icon = employer.logoUrls?.logoUrl.orEmpty()
+        employer = employer?.name.orEmpty(),
+        area = area?.name.orEmpty(),
+        experience = experience?.name.orEmpty(),
+        employment = employment?.name.orEmpty(),
+        workFormat = workFormat?.map { it.name }.orEmpty(),
+        description = description.orEmpty(),
+        keySkills = keySkills?.map { it.name }.orEmpty(),
+        icon = employer?.logoUrls?.logoUrl.orEmpty()
     )
 }
