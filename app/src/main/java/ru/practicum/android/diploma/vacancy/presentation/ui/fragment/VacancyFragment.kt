71046f33b85e864fd5c3b382746335d965cffee1
SergeySh97 <Sergey_Sh97@mail.ru>
2025-02-24 10:39:26 +0300

    GH-45: Добавление методов для получения деталей вакансии

--- app/src/main/java/ru/practicum/android/diploma/vacancy/presentation/ui/fragment/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/presentation/ui/fragment/VacancyFragment.kt
@@ -1,19 +1,26 @@
 package ru.practicum.android.diploma.vacancy.presentation.ui.fragment
 
+import android.util.Log
+import androidx.navigation.fragment.navArgs
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.core.ui.BaseFragment
 import ru.practicum.android.diploma.databinding.FragmentVacancyBinding
 import ru.practicum.android.diploma.vacancy.presentation.viewmodel.VacancyViewModel
 
-class VacancyFragment(
-    override val viewModel: VacancyViewModel
-) : BaseFragment<FragmentVacancyBinding, VacancyViewModel>(
+class VacancyFragment : BaseFragment<FragmentVacancyBinding, VacancyViewModel>(
     inflate = FragmentVacancyBinding::inflate
 ) {
+    override val viewModel: VacancyViewModel by viewModel()
+    private val args by navArgs<VacancyFragmentArgs>()
+    private val vacancyId by lazy { args.vacancyId }
+
     override fun initViews() {
-        TODO("Not yet implemented")
+        viewModel.getVacancy(vacancyId.toInt())
     }
 
     override fun subscribe() {
-        TODO("Not yet implemented")
+        viewModel.observeVacancy.observe(viewLifecycleOwner) {
+            Log.d("VacancyFragment", "Vacancy: $it")
+        }
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/vacancy/presentation/viewmodel/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/presentation/viewmodel/VacancyViewModel.kt
@@ -1,5 +1,22 @@
 package ru.practicum.android.diploma.vacancy.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.core.ui.BaseViewModel
+import ru.practicum.android.diploma.vacancy.domain.model.VacancyDetails
+import ru.practicum.android.diploma.vacancy.domain.usecase.GetVacancyUseCase
 
-class VacancyViewModel : BaseViewModel()
+class VacancyViewModel(private val getVacancyUseCase: GetVacancyUseCase) : BaseViewModel() {
+
+    private val vacancyLiveData = MutableLiveData<VacancyDetails>()
+
+    val observeVacancy: LiveData<VacancyDetails> = vacancyLiveData
+
+    fun getVacancy(vacancyId: Int) {
+        viewModelScope.launch {
+            vacancyLiveData.value = getVacancyUseCase.execute(vacancyId)
+        }
+    }
+}
