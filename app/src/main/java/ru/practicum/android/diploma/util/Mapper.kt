8c5c97aa27588ceef8a38a4e836b80172fc13fb5
SergeySh97 <Sergey_Sh97@mail.ru>
2025-02-25 21:14:41 +0300

    GH-53: создание мапера для ошибок

--- app/src/main/java/ru/practicum/android/diploma/util/Mapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/Mapper.kt
@@ -1,6 +1,8 @@
 package ru.practicum.android.diploma.util
 
 import ru.practicum.android.diploma.search.domain.model.Vacancy
+import ru.practicum.android.diploma.util.Constants.NOT_FOUND_CODE
+import ru.practicum.android.diploma.vacancy.domain.model.ErrorType
 import ru.practicum.android.diploma.vacancy.domain.model.VacancyDetails
 
 fun VacancyDetails.toVacancy(): Vacancy = Vacancy(
@@ -13,4 +15,11 @@ fun VacancyDetails.toVacancy(): Vacancy = Vacancy(
     area = this.area,
     icon = this.icon
 )
+
+fun Int.mapToErrorType(): ErrorType {
+    return when (this) {
+        NOT_FOUND_CODE -> ErrorType.NOT_FOUND
+        else -> ErrorType.UNKNOWN
+    }
+}
 
