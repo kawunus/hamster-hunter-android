3f27f3d0328c39a7045e4e112c667bd2641db9db
Svitlana Filippova <s.rubinets@gmail.com>
2025-02-26 11:02:12 +0300

    GH-70 правки от Сергея

--- app/src/main/java/ru/practicum/android/diploma/favorites/domain/usecase/FavoriteVacancyInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/usecase/FavoriteVacancyInteractor.kt
@@ -1,4 +1,4 @@
-package ru.practicum.android.diploma.favorites.domain.api
+package ru.practicum.android.diploma.favorites.domain.usecase
 
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.search.domain.model.Vacancy
--- app/src/main/java/ru/practicum/android/diploma/search/data/dto/VacancyDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/dto/VacancyDto.kt
@@ -1,9 +1,9 @@
 package ru.practicum.android.diploma.search.data.dto
 
 data class VacancyDto(
-    val id: String?, // Идентификатор вакансии
-    val name: String?, // Название вакансии
-    val salary: Salary?, // Зарплата
-    val employer: Employer?, // Информация о компании-работодателе
-    val area: Area? // Регион
+    val id: String?,
+    val name: String?,
+    val salary: Salary?,
+    val employer: Employer?,
+    val area: Area?
 )
