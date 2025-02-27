db769c10d143d15f37c4895fc0523e2560032845
Svitlana Filippova <s.rubinets@gmail.com>
2025-02-27 14:23:06 +0300

    GH-76 нейминг фрагментов Место работы и Отрасль

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/ui/fragment/AreaFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/ui/fragment/AreaFragment.kt
@@ -2,11 +2,12 @@ package ru.practicum.android.diploma.filter.presentation.ui.fragment
 
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.core.ui.BaseFragment
-import ru.practicum.android.diploma.databinding.FragmentWorkplaceBinding
+import ru.practicum.android.diploma.databinding.FragmentAreaBinding
+
 import ru.practicum.android.diploma.filter.presentation.viewmodel.WorkplaceViewModel
 
-class WorkplaceFragment : BaseFragment<FragmentWorkplaceBinding, WorkplaceViewModel>(
-    inflate = FragmentWorkplaceBinding::inflate
+class AreaFragment : BaseFragment<FragmentAreaBinding, WorkplaceViewModel>(
+    inflate = FragmentAreaBinding::inflate
 ) {
     override val viewModel: WorkplaceViewModel by viewModel()
     override fun initViews() {
--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/ui/fragment/IndustryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/ui/fragment/IndustryFragment.kt
@@ -2,11 +2,11 @@ package ru.practicum.android.diploma.filter.presentation.ui.fragment
 
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.core.ui.BaseFragment
-import ru.practicum.android.diploma.databinding.FragmentFieldBinding
+import ru.practicum.android.diploma.databinding.FragmentIndustryBinding
 import ru.practicum.android.diploma.filter.presentation.viewmodel.FieldViewModel
 
-class FieldFragment : BaseFragment<FragmentFieldBinding, FieldViewModel>(
-    inflate = FragmentFieldBinding::inflate
+class IndustryFragment : BaseFragment<FragmentIndustryBinding, FieldViewModel>(
+    inflate = FragmentIndustryBinding::inflate
 ) {
     override val viewModel: FieldViewModel by viewModel()
     override fun initViews() {
