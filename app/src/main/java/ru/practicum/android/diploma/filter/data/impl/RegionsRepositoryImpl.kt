cb3761798c63ba186b0dccaa100893e1b7ed42bc
Александр А. <laorha@yandex.ru>
2025-03-01 23:48:45 +0500

    GH-80-83: Исправлены плейсхолдеры исключений

--- app/src/main/java/ru/practicum/android/diploma/filter/data/impl/RegionsRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/data/impl/RegionsRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.filter.data.impl
 
+import android.content.Context
 import android.util.Log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -10,79 +11,92 @@ import ru.practicum.android.diploma.filter.data.network.model.RegionsResponse
 import ru.practicum.android.diploma.filter.domain.api.RegionsRepository
 import ru.practicum.android.diploma.filter.domain.models.Region
 import ru.practicum.android.diploma.util.Constants
+import ru.practicum.android.diploma.util.NetworkMonitor
 import ru.practicum.android.diploma.util.Resource
 import ru.practicum.android.diploma.util.toRegion
 import java.io.IOException
+import java.net.SocketTimeoutException
 
 class RegionsRepositoryImpl(
-    private val networkClient: NetworkClient
+    private val networkClient: NetworkClient,
+    private val context: Context
 ) : RegionsRepository {
+
     override suspend fun getRegions(countryId: String): Flow<Resource<List<Region>>> = flow {
         try {
-            var response = networkClient.doRequest(RegionsRequest(countryId))
-            Log.e("RegionSearch", "Repo код: ${response.resultCode}")
+            if (!isConnected()) {
+                emit(Resource(data = null, code = -1))
+            }
+
+            val response = networkClient.doRequest(RegionsRequest(countryId))
             if (response !is RegionsResponse) {
                 emit(Resource(data = null, code = Constants.HTTP_BAD_REQUEST))
             } else {
                 when (response.resultCode) {
                     Constants.HTTP_SUCCESS -> {
-                        response = response as RegionsResponse
                         val regions = response.regionsList.map { it.toRegion() }
                         emit(Resource(data = regions, code = Constants.HTTP_SUCCESS))
                     }
 
-                    Constants.HTTP_NOT_FOUND -> {
-                        emit(Resource(data = null, code = Constants.HTTP_NOT_FOUND))
-                    }
+                    Constants.HTTP_NOT_FOUND -> emit(Resource(data = null, code = Constants.HTTP_NOT_FOUND))
+                    Constants.HTTP_BAD_REQUEST, Constants.HTTP_SERVER_ERROR -> emit(
+                        Resource(
+                            data = null,
+                            code = Constants.HTTP_SERVER_ERROR
+                        )
+                    )
 
-                    Constants.HTTP_BAD_REQUEST, Constants.HTTP_SERVER_ERROR -> {
-                        emit(Resource(data = null, code = Constants.HTTP_SERVER_ERROR))
-                    }
-
-                    -1 -> {
-                        emit(Resource(data = null, code = -1))
-                    }
-
-                    else -> {
-                        emit(Resource(data = null, code = response.resultCode))
-                    }
+                    -1 -> emit(Resource(data = null, code = -1))
+                    else -> emit(Resource(data = null, code = response.resultCode))
                 }
             }
+        } catch (e: SocketTimeoutException) {
+            Log.e("RegionsRepositoryImpl", "Таймаут запроса: ${e.localizedMessage}", e)
+            emit(Resource(data = null, code = Constants.HTTP_SERVER_ERROR))
         } catch (e: IOException) {
             Log.e("RegionsRepositoryImpl", "Ошибка сети: ${e.localizedMessage}", e)
             emit(Resource(data = null, code = -1))
         }
     }
+
     override suspend fun getAllRegions(): Flow<Resource<List<Region>>> = flow {
         try {
-            var response = networkClient.doRequest(AllRegionsRequest())
+            if (!isConnected()) {
+                emit(Resource(data = null, code = -1))
+            }
+
+            val response = networkClient.doRequest(AllRegionsRequest())
             if (response !is RegionsResponse) {
                 emit(Resource(data = null, code = Constants.HTTP_BAD_REQUEST))
             } else {
                 when (response.resultCode) {
                     Constants.HTTP_SUCCESS -> {
-                        response = response as RegionsResponse
                         val regions = response.regionsList.map { it.toRegion() }
                         emit(Resource(data = regions, code = Constants.HTTP_SUCCESS))
                     }
-                    Constants.HTTP_NOT_FOUND -> {
-                        emit(Resource(data = null, code = Constants.HTTP_NOT_FOUND))
-                    }
-                    Constants.HTTP_BAD_REQUEST, Constants.HTTP_SERVER_ERROR -> {
-                        emit(Resource(data = null, code = Constants.HTTP_SERVER_ERROR))
-                    }
-                    -1 -> {
-                        emit(Resource(data = null, code = -1))
-                    }
-                    else -> {
-                        emit(Resource(data = null, code = response.resultCode))
-                    }
+
+                    Constants.HTTP_NOT_FOUND -> emit(Resource(data = null, code = Constants.HTTP_NOT_FOUND))
+                    Constants.HTTP_BAD_REQUEST, Constants.HTTP_SERVER_ERROR -> emit(
+                        Resource(
+                            data = null,
+                            code = Constants.HTTP_SERVER_ERROR
+                        )
+                    )
+
+                    -1 -> emit(Resource(data = null, code = -1))
+                    else -> emit(Resource(data = null, code = response.resultCode))
                 }
             }
+        } catch (e: SocketTimeoutException) {
+            Log.e("RegionsRepositoryImpl", "Таймаут запроса: ${e.localizedMessage}", e)
+            emit(Resource(data = null, code = Constants.HTTP_SERVER_ERROR))
         } catch (e: IOException) {
             Log.e("RegionsRepositoryImpl", "Ошибка сети: ${e.localizedMessage}", e)
             emit(Resource(data = null, code = -1))
         }
     }
 
+    private fun isConnected(): Boolean {
+        return NetworkMonitor.isNetworkAvailable(context)
+    }
 }
