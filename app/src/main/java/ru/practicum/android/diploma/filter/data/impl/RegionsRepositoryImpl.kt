a3af0429f43522cee7124f14d1dd47b2a9882885
Александр А. <laorha@yandex.ru>
2025-03-02 00:00:18 +0500

    GH-80-83: Кодстайл

--- app/src/main/java/ru/practicum/android/diploma/filter/data/impl/RegionsRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/data/impl/RegionsRepositoryImpl.kt
@@ -18,43 +18,41 @@ import java.io.IOException
 import java.net.SocketTimeoutException
 
 class RegionsRepositoryImpl(
-    private val networkClient: NetworkClient,
-    private val context: Context
+    private val networkClient: NetworkClient, private val context: Context
 ) : RegionsRepository {
 
     override suspend fun getRegions(countryId: String): Flow<Resource<List<Region>>> = flow {
         try {
             if (!isConnected()) {
                 emit(Resource(data = null, code = -1))
-            }
-
-            val response = networkClient.doRequest(RegionsRequest(countryId))
-            if (response !is RegionsResponse) {
-                emit(Resource(data = null, code = Constants.HTTP_BAD_REQUEST))
             } else {
-                when (response.resultCode) {
-                    Constants.HTTP_SUCCESS -> {
-                        val regions = response.regionsList.map { it.toRegion() }
-                        emit(Resource(data = regions, code = Constants.HTTP_SUCCESS))
-                    }
+                val response = networkClient.doRequest(RegionsRequest(countryId))
+                if (response !is RegionsResponse) {
+                    emit(Resource(data = null, code = Constants.HTTP_BAD_REQUEST))
+                } else {
+                    when (response.resultCode) {
+                        Constants.HTTP_SUCCESS -> {
+                            val regions = response.regionsList.map { it.toRegion() }
+                            emit(Resource(data = regions, code = Constants.HTTP_SUCCESS))
+                        }
 
-                    Constants.HTTP_NOT_FOUND -> emit(Resource(data = null, code = Constants.HTTP_NOT_FOUND))
-                    Constants.HTTP_BAD_REQUEST, Constants.HTTP_SERVER_ERROR -> emit(
-                        Resource(
-                            data = null,
-                            code = Constants.HTTP_SERVER_ERROR
+                        Constants.HTTP_NOT_FOUND -> emit(Resource(data = null, code = Constants.HTTP_NOT_FOUND))
+                        Constants.HTTP_BAD_REQUEST, Constants.HTTP_SERVER_ERROR -> emit(
+                            Resource(
+                                data = null, code = Constants.HTTP_SERVER_ERROR
+                            )
                         )
-                    )
 
-                    -1 -> emit(Resource(data = null, code = -1))
-                    else -> emit(Resource(data = null, code = response.resultCode))
+                        -1 -> emit(Resource(data = null, code = -1))
+                        else -> emit(Resource(data = null, code = response.resultCode))
+                    }
                 }
             }
         } catch (e: SocketTimeoutException) {
-            Log.e("RegionsRepositoryImpl", "Таймаут запроса: ${e.localizedMessage}", e)
+            Log.e(TAG, "Таймаут запроса: ${e.localizedMessage}", e)
             emit(Resource(data = null, code = Constants.HTTP_SERVER_ERROR))
         } catch (e: IOException) {
-            Log.e("RegionsRepositoryImpl", "Ошибка сети: ${e.localizedMessage}", e)
+            Log.e(TAG, "Ошибка сети: ${e.localizedMessage}", e)
             emit(Resource(data = null, code = -1))
         }
     }
@@ -63,40 +61,43 @@ class RegionsRepositoryImpl(
         try {
             if (!isConnected()) {
                 emit(Resource(data = null, code = -1))
-            }
-
-            val response = networkClient.doRequest(AllRegionsRequest())
-            if (response !is RegionsResponse) {
-                emit(Resource(data = null, code = Constants.HTTP_BAD_REQUEST))
             } else {
-                when (response.resultCode) {
-                    Constants.HTTP_SUCCESS -> {
-                        val regions = response.regionsList.map { it.toRegion() }
-                        emit(Resource(data = regions, code = Constants.HTTP_SUCCESS))
-                    }
+                val response = networkClient.doRequest(AllRegionsRequest())
+                if (response !is RegionsResponse) {
+                    emit(Resource(data = null, code = Constants.HTTP_BAD_REQUEST))
+                } else {
+                    when (response.resultCode) {
+                        Constants.HTTP_SUCCESS -> {
+                            val regions = response.regionsList.map { it.toRegion() }
+                            emit(Resource(data = regions, code = Constants.HTTP_SUCCESS))
+                        }
 
-                    Constants.HTTP_NOT_FOUND -> emit(Resource(data = null, code = Constants.HTTP_NOT_FOUND))
-                    Constants.HTTP_BAD_REQUEST, Constants.HTTP_SERVER_ERROR -> emit(
-                        Resource(
-                            data = null,
-                            code = Constants.HTTP_SERVER_ERROR
+                        Constants.HTTP_NOT_FOUND -> emit(Resource(data = null, code = Constants.HTTP_NOT_FOUND))
+                        Constants.HTTP_BAD_REQUEST, Constants.HTTP_SERVER_ERROR -> emit(
+                            Resource(
+                                data = null, code = Constants.HTTP_SERVER_ERROR
+                            )
                         )
-                    )
 
-                    -1 -> emit(Resource(data = null, code = -1))
-                    else -> emit(Resource(data = null, code = response.resultCode))
+                        -1 -> emit(Resource(data = null, code = -1))
+                        else -> emit(Resource(data = null, code = response.resultCode))
+                    }
                 }
             }
         } catch (e: SocketTimeoutException) {
-            Log.e("RegionsRepositoryImpl", "Таймаут запроса: ${e.localizedMessage}", e)
+            Log.e(TAG, "Таймаут запроса: ${e.localizedMessage}", e)
             emit(Resource(data = null, code = Constants.HTTP_SERVER_ERROR))
         } catch (e: IOException) {
-            Log.e("RegionsRepositoryImpl", "Ошибка сети: ${e.localizedMessage}", e)
+            Log.e(TAG, "Ошибка сети: ${e.localizedMessage}", e)
             emit(Resource(data = null, code = -1))
         }
     }
 
     private fun isConnected(): Boolean {
         return NetworkMonitor.isNetworkAvailable(context)
+    }
+
+    companion object {
+        private const val TAG = "RegionsRepositoryImpl"
     }
 }
