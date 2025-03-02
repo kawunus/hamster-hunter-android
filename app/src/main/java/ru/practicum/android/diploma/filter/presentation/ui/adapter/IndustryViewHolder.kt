a3b33927d703702afa8fe8cb965f518cfd557e68
stanislau <s.markovtsev@gmail.com>
2025-03-02 20:50:10 +0300

    GH-74: поправил кодстайл

--- app/src/main/java/ru/practicum/android/diploma/filter/presentation/ui/adapter/IndustryViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/presentation/ui/adapter/IndustryViewHolder.kt
@@ -5,11 +5,9 @@ import ru.practicum.android.diploma.databinding.ItemIndustryBinding
 import ru.practicum.android.diploma.filter.domain.model.Industry
 
 class IndustryViewHolder(
-    private val binding:ItemIndustryBinding,
+    private val binding: ItemIndustryBinding,
     private val onItemClicked: (Industry) -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
-
-
 
     fun bind(industry: Industry) {
         // Устанавливаем название отрасли
