946e14b6edf0e6c918fa42e03424e62463b7b140
stanislau <s.markovtsev@gmail.com>
2025-03-02 21:34:28 +0300

    GH-71: Добавил интерфейсы для получения списка отраслей

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/domain/usecase/IndustriesInteractor.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.filter.domain.usecase
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.filter.domain.model.Industry
+import ru.practicum.android.diploma.util.Resource
+
+interface IndustriesInteractor {
+
+    suspend fun getAllIndustries(): Flow<Resource<List<Industry>>>
+}
